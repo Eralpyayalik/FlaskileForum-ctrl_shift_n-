32a6dab089de45e79bc84e18b8ac01123cc8285d
Eralp Yayalık <eralpyayalikk@gmail.com>
2026-02-01 23:37:47 +0300

    Veritabanı ayarları ve gereksinimler güncellendi

--- blog.py
+++ blog.py
@@ -63,6 +63,8 @@ app.config["MYSQL_USER"] = os.getenv("MYSQL_USER")
 app.config["MYSQL_PASSWORD"] = os.getenv("MYSQL_PASSWORD")
 app.config["MYSQL_DB"] = os.getenv("MYSQL_DB")
 app.config["MYSQL_PORT"] = int(os.getenv("MYSQL_PORT", 26850))
+app.config["MYSQL_CURSORCLASS"] = "DictCursor" # Zaten vardır muhtemelen
+app.config["MYSQL_CUSTOM_OPTIONS"] = {"ssl": {"ca": "/etc/ssl/certs/ca-certificates.crt"}}
 
 mysql=MySQL(app)
 
