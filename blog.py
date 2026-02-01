09429ed93d87985f5a93fe13e35bca239b401730
Eralp Yayalık <eralpyayalikk@gmail.com>
2026-02-01 22:40:07 +0300

    veritabani portu ve config guncellendi

--- blog.py
+++ blog.py
@@ -58,13 +58,11 @@ class LoginForm(Form):
 
 app = Flask(__name__)
 
-app.config["UPLOAD_FOLDER"] = "static/avatars"
-app.secret_key="ybblog"
-app.config["MYSQL_HOST"]="localhost"
-app.config["MYSQL_USER"]="root"
-app.config["MYSQL_PASSWORD"]=""
-app.config["MYSQL_DB"]="ybblog"
-app.config["MYSQL_CURSORCLASS"]="DictCursor"
+app.config["MYSQL_HOST"] = os.getenv("MYSQL_HOST")
+app.config["MYSQL_USER"] = os.getenv("MYSQL_USER")
+app.config["MYSQL_PASSWORD"] = os.getenv("MYSQL_PASSWORD")
+app.config["MYSQL_DB"] = os.getenv("MYSQL_DB")
+app.config["MYSQL_PORT"] = int(os.getenv("MYSQL_PORT", 26850))
 
 mysql=MySQL(app)
 
